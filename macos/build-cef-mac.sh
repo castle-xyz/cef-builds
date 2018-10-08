31c2136f42f076237158e3402cf0c4936636fa79
Ben Roth <spongebucket@gmail.com>
2018-10-08 15:43:41 -0700

    macos: update build script to use cef build tooling and correct flags

--- macos/build-cef-mac.sh
+++ macos/build-cef-mac.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "hello world"
+export CEF_USE_GN=1
+export GN_DEFINES="is_official_build=true proprietary_codecs=true ffmpeg_branding=Chrome"
+export CEF_ARCHIVE_FORMAT=tar.bz2
+python ../common/automate-git.py --download-dir=download/chromium_git --depot-tools-dir=download/depot_tools --x64-build --no-debug-build --branch=3497
+
