35dbefd1f8251ba7e8b98d802a60032f39dadf94
Ben Roth <spongebucket@gmail.com>
2018-10-11 13:19:12 -0700

    wip: adjust paths and allow building

--- macos/build-cef-mac.sh
+++ macos/build-cef-mac.sh
@@ -3,5 +3,5 @@
 export CEF_USE_GN=1
 export GN_DEFINES="is_official_build=true proprietary_codecs=true ffmpeg_branding=Chrome"
 export CEF_ARCHIVE_FORMAT=tar.bz2
-python ../common/automate-git.py --download-dir=~/project/download/chromium_git --depot-tools-dir=~/project/download/depot_tools --no-distrib --no-build --x64-build --branch=3497
-echo "this script did something"
+python ../common/automate-git.py --download-dir=download/chromium_git --depot-tools-dir=download/depot_tools --x64-build --branch=3497
+
